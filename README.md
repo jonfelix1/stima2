a7a98bd1cfcbc46c8bba8e66eb9cc75ab1c50956
Jon Felix Germinian <jonfelix1@gmail.com>
2020-03-12 14:46:22 +0700

    modification to use tuple instead

--- README.md
+++ README.md
@@ -5,9 +5,8 @@
 | Gunawan K        | 13518103 |
 | Jon Felix G      | 13518025 |
 
-How to run the code
-
-cd to `covid19` then run `dotnet run`
 
 Also 
+
+
 ![alt text](https://s1.zerochan.net/Lancer.%28Ereshkigal%29.600.2303142.jpg "We Must Protect Her Smile")
--- covid19/Program.cs
+++ covid19/Program.cs
@@ -5,40 +5,40 @@
 using System;
 using System.Collections.Generic;
 
-class CityNeighbor{
-    private City city;
-    private int weight;
+// class Tuple<City,int>{
+//     private City city;
+//     private int weight;
 
-    public CityNeighbor(City a, int weight){
-        this.city = a;
-        this.weight = weight;
-    }
+//     public Tuple<City,int>(City a, int weight){
+//         this.city = a;
+//         this.weight = weight;
+//     }
 
-    public int Weight{
-        get{
-            return this.weight;
-        }
-        set{
-            this.weight = value;
-        }
-    }
-}
+//     public int Weight{
+//         get{
+//             return this.weight;
+//         }
+//         set{
+//             this.weight = value;
+//         }
+//     }
+// }
 
 class City{
     private int population = 0; //buat populasi
     private int T = -1; //waktu pertama kota diinfeksi
-    private List<CityNeighbor> neighbors;
+    private List<Tuple<City,Double>> neighbors;
 
     //ctor
     // Inisiasi cuman pake value, add neighbor pake addneighbors atau neighbors = 
 
     public City(){
-        this.neighbors =  new List<CityNeighbor>();
+        this.neighbors =  new List<Tuple<City,Double>>();
 
     }
     public City(int pop){
         this.population = pop;
-        this.neighbors = new List<CityNeighbor>();
+        this.neighbors = new List<Tuple<City,Double>>();
     }
 
     //cctor gk perlu
@@ -56,7 +56,7 @@ class City{
         }
     }
 
-    public List<CityNeighbor> Neighbors {
+    public List<Tuple<City,Double>> Neighbors {
         get{
             return this.neighbors;
         }
@@ -71,7 +71,7 @@ class City{
         }
     }
 
-    public int SetInfectedTime{
+    public int InfectedTime{
         get{
             return this.T;
         }
@@ -80,9 +80,14 @@ class City{
         }
     }
 
+    public double Tr(City b){
+        double temp = this.neighbors.Find(x => x.Item1 == b).Item2;
+        return temp;
+    }
+
     // Nambahin 1 tetangga
     public void AddNeighbor(City a, int weight){
-        CityNeighbor temp = new CityNeighbor(a, weight);
+        Tuple<City,double> temp = new Tuple<City,double>(a, weight);
         this.neighbors.Add(temp);
     }
 
@@ -120,11 +125,17 @@ class CityGraph{
         this.citylist.Add(a);
     }
 
+    public double S(City a, City b, int timenow){
+        return a.I(timenow)*a.Tr(b);
+    }
+
     // Implementassin BFS disini
+    // Fungsi menerima reference queue
 }
 
 class MainProgram{
     public static void Main(String[] args){
+        Queue<Tuple<City,City>> sQueue = new Queue<Tuple<City, City>>();
         City a =  new City(900);
 
         Console.WriteLine("Testing Successful");
